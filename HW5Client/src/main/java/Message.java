7cbf6444602d9ddddf4e3acb377f0406ff039a40
BenG <92496666+Fei302@users.noreply.github.com>
2026-04-09 02:55:08 -0500

    Message - Erased anything to do with grouping as that is not needed for this HW.

--- HW5Client/src/main/java/Message.java
+++ HW5Client/src/main/java/Message.java
@@ -2,4 +2,15 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
     static final long serialVersionUID = 42L;
+
+    public enum MessageType {
+        REGISTER,
+        SEND_MESSAGE_TO_ALL_CLIENTS,
+        SEND_MESSAGE_TO_ONE_CLIENT
+    }
+
+    // MessageToBeSent - meant only for the send messages action and register
+    // Sender - Meant for send messages actions, and register
+    // Receiver - meant messages actions
+    String messageToBeSent, sender, receiver;
 }
