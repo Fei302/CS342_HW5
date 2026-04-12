fb9be4c991ce13f0c7f8cea97d0d1441d56d4995
BenG <92496666+Fei302@users.noreply.github.com>
2026-04-12 02:03:18 -0500

    Updated Message to include userList and Error in case an error happens

--- HW5Client/src/main/java/Message.java
+++ HW5Client/src/main/java/Message.java
@@ -1,16 +1,24 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Message implements Serializable {
     static final long serialVersionUID = 42L;
 
     public enum MessageType {
-        REGISTER,
+        REGISTER,                   // Client -> Server: "I want this username"
+        ERROR,                      // Server -> Client: "Something went wrong"
+        USER_LIST,                  // Server -> All Clients: updated list of connected users
         SEND_MESSAGE_TO_ALL_CLIENTS,
         SEND_MESSAGE_TO_ONE_CLIENT
     }
 
-    // MessageToBeSent - meant only for the send messages action and register
-    // Sender - Meant for send messages actions, and register
-    // Receiver - meant messages actions
-    String messageToBeSent, sender, receiver;
+    MessageType type;               // What kind of message is this?
+    String messageToBeSent;         // The actual text content
+    String sender;                  // Who sent it
+    String receiver;                // Who it's going to (only for SEND_MESSAGE_TO_ONE_CLIENT)
+    ArrayList<String> userList;     // Only used for USER_LIST type
+
+    Message(MessageType type) {
+        this.type = type;
+    }
 }
