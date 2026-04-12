3c7266adf973f950e544a6b3df0ab32e97939b90
BenG <92496666+Fei302@users.noreply.github.com>
2026-04-12 02:10:28 -0500

    Updated Server GUI in order to properly display logs from users via a Hashmap

--- HW5Server/src/main/java/GuiServer.java
+++ HW5Server/src/main/java/GuiServer.java
@@ -1,74 +1,57 @@
-
-import java.util.HashMap;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-public class GuiServer extends Application{
+import java.util.HashMap;
+
+public class GuiServer extends Application {
 
 	HashMap<String, Scene> sceneMap;
 	Server serverConnection;
-	
 	ListView<String> listItems;
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		listItems = new ListView<>();
+
+		// Log activity to the GUI
 		serverConnection = new Server(data -> {
-			Platform.runLater(()->{
+			Platform.runLater(() -> {
 				listItems.getItems().add(data.toString());
 			});
 		});
 
-		
-		listItems = new ListView<String>();
+		sceneMap = new HashMap<>();
+		sceneMap.put("server", createServerGui());
 
-		sceneMap = new HashMap<String, Scene>();
-		
-		sceneMap.put("server",  createServerGui());
-		
 		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
+			@Override
+			public void handle(WindowEvent t) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
 
 		primaryStage.setScene(sceneMap.get("server"));
-		primaryStage.setTitle("This is the Server");
+		primaryStage.setTitle("Server Log");
 		primaryStage.show();
-		
 	}
-	
+
 	public Scene createServerGui() {
-		
 		BorderPane pane = new BorderPane();
-		pane.setPadding(new Insets(70));
-		pane.setStyle("-fx-background-color: coral");
-		
+		pane.setPadding(new Insets(20));
+		pane.setStyle("-fx-background-color: coral; -fx-font-family: 'serif';");
 		pane.setCenter(listItems);
-		pane.setStyle("-fx-font-family: 'serif'");
 		return new Scene(pane, 500, 400);
-		
-		
 	}
-
-
 }
